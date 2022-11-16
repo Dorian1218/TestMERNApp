8e5a5249006ad8cad7b82a190432ea9514d7400b
Dorian1218 <dmitchell1663@gmail.com>
2022-11-16 16:51:44 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const UserModel = require("./models/Users")
+require("dotenv").config()
+const uri = process.env.ATLAS_URI
+
+app.use(express.json())
+
+mongoose.connect(uri)
+
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if (err) {
+            res.json(err)
+        } else {
+            res.json(result)
+        }
+    })
+})
+
+app.post("/createUsers", async (req, res) => {
+    const user = req.body
+    const newUser = new UserModel(user)
+    await newUser.save()
+
+    res.json(user)
+})
+
+app.listen(3001, () => {
+    console.log("SERVER IS RUNNING ON PORT " + 3001)
+})
