b9277eaae6862957d31917f890d3e50153795b03
Dorian1218 <dmitchell1663@gmail.com>
2023-03-13 21:03:31 -0400

    added header and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "./App.css"
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import { AuthContextProvider } from "./AuthContext";
 
 function App() {
+  const navigate = useNavigate()
 
   return (
     <AuthContextProvider>
+      <div className="navbar">
+          <Link to="/"><li>Notes App</li></Link>
+        <div>
+        <Link to="/signup"><button className="signup-button">Signup</button></Link>
+        <button className="modal-button">Signin</button>
+        </div>
+      </div>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/signup" element={<Signup />}></Route>
