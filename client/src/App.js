1d02dcd4e1b41fc2e5f5df66d22627c1ba5eb05e
Dorian1218 <dmitchell1663@gmail.com>
2023-03-14 22:07:59 -0400

    added signin page and changed styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, useNavigate, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import { AuthContextProvider } from "./AuthContext";
+import Signin from "./pages/Signin";
 
 function App() {
   const navigate = useNavigate()
@@ -13,13 +14,15 @@ function App() {
       <div className="navbar">
           <Link to="/"><li>Notes App</li></Link>
         <div>
-        <Link to="/signup"><button className="signup-button">Signup</button></Link>
-        <button className="modal-button">Signin</button>
+        <Link to="/signin">Sign in</Link>
+        <Link to="/signup"><button className="signup-button">Sign up</button></Link>
+        {/* <button className="modal-button">Sign in</button> */}
         </div>
       </div>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
       </Routes>
       </AuthContextProvider>
   );
---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Signin() {
+  return (
+    <div>Signin now</div>
+  )
+}
+
+export default Signin
